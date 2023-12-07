8170921dc4570ed05c5e10c00c20fda9a2dcdbcc
Nicolai Wagner <nocwagner@gmail.com>
2023-12-07 16:03:12 +0100

    Bestillingsmodal og hygge

--- components/frontpage/MenuItem.jsx
+++ components/frontpage/MenuItem.jsx
@@ -1,15 +1,23 @@
 import Image from "next/image";
 
-export default function MenuItem(props) {
+export default function MenuItem({
+  status,
+  src,
+  title,
+  amount,
+  price,
+  onClick,
+}) {
   return (
     <div
+      onClick={() => onClick({ status, src, title, amount, price, onClick })}
       className={`flex bg-white shadow-xl p-4 rounded-lg w-11/12 md:w-2/5 cursor-pointer ${
-        props.status === "sold_out" && "opacity-50 pointer-events-none"
+        status === "sold_out" && "opacity-50 pointer-events-none"
       }`}
     >
       <div className="w-1/2">
         <Image
-          src={props.src}
+          src={src}
           height={200}
           width={200}
           alt="Billede af Birriatacos"
@@ -17,13 +25,13 @@ export default function MenuItem(props) {
         />
       </div>
       <div className="w-1/2">
-        <h3 className="font-bold text-xl pb-2">{props.title}</h3>
+        <h3 className="font-bold text-xl pb-2">{title}</h3>
         <p className="line-clamp-3">
-          {props.amount} velsmagende tacos med hjemmelavet langtids braiseret
-          oksekød. Serveret med ost, løg, koriander eller persille og lime.
+          {amount} velsmagende tacos med hjemmelavet langtids braiseret oksekød.
+          Serveret med ost, løg, koriander eller persille og lime.
         </p>
         <div className="flex pt-2 items-center justify-between">
-          <p className="text-lg">kr. {props.price},-</p>
+          <p className="text-lg">kr. {price},-</p>
           <button className="bg-light-orange p-2 pl-4 pr-4 rounded-lg">
             Tilføj
           </button>
